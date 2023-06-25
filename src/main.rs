f3bbccb3a8d5c20cc9cc75203908ba5ccb9bcc38
Khai Shern, Toh <55418374+jamestoh-leos@users.noreply.github.com>
2023-06-25 17:53:06 +0800

    Add a check when user entering a voice channel is a bot and exclude from notification.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::model::id::{ChannelId, RoleId};
 use serenity::model::voice::VoiceState;
 use serenity::prelude::*;
 
@@ -18,9 +19,64 @@ impl EventHandler for Handler {
         }
     }
 
-    async fn voice_state_update(&self, _ctx: Context, old: Option<VoiceState>, new: VoiceState) {
-        println!("N.. {:?}", new);
-        println!("O.. {:?}", old);
+    async fn voice_state_update(&self, ctx: Context, old: Option<VoiceState>, new: VoiceState) {
+        if new.member.as_ref().unwrap().user.bot {
+            return;
+        }
+        let text_channel = ChannelId(890090820613603409);
+        let mention = RoleId(889106903836286987).mention();
+
+        if let Some(new_channel_id) = new.channel_id {
+            let user_name = new.member.as_ref().unwrap().display_name();
+            let channel_name = match new_channel_id.name(&ctx.cache).await {
+                Some(name) => name,
+                None => "unknown".to_string(),
+            };
+
+            // Check if user moved from another channel or not
+            if let Some(old_channel_id) = old.and_then(|v| v.channel_id) {
+                // User moved from old channel to new channel
+                if new_channel_id != old_channel_id {
+                    let old_channel_name = match old_channel_id.name(&ctx.cache).await {
+                        Some(name) => name,
+                        None => "Unkown".to_string(),
+                    };
+                    let msg = format!(
+                        "{}, {} moved from {} to {}",
+                        mention, user_name, old_channel_name, channel_name
+                    );
+                    // Send message to text channel
+                    if let Err(why) = text_channel.say(&ctx.http, msg).await {
+                        println!("{:?}", why);
+                    };
+                }
+            } else {
+                // User joined new channel
+                let msg = format!("{}, {} joined {}", mention, user_name, channel_name);
+                // Send message to text channel
+                if let Err(why) = text_channel.say(&ctx.http, msg).await {
+                    println!("{:?}", why);
+                };
+            }
+        } else if let Some(old_channel_id) = old.as_ref().and_then(|v| v.channel_id) {
+            // User left a voice channel
+            let user_name = old
+                .as_ref()
+                .unwrap()
+                .member
+                .as_ref()
+                .unwrap()
+                .display_name();
+            let channel_name = match old_channel_id.name(&ctx.cache).await {
+                Some(name) => name,
+                None => "Unknown".to_string(),
+            };
+            let msg = format!("{}, {} left {}", mention, user_name, channel_name);
+            // Send message to text channel
+            if let Err(why) = text_channel.say(&ctx.http, msg).await {
+                println!("{:?}", why);
+            }
+        }
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
