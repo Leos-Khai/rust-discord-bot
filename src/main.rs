e1430405c2d2629fad422fdb4cdadfdbb1ed7e54
Khai Shern, Toh <khaishern8235@gmail.com>
2024-03-06 15:22:41 +0800

    Added file  include for channel_id and role_id for easier distribution.

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,20 @@ use serenity::model::id::{ChannelId, RoleId};
 use serenity::model::voice::VoiceState;
 use serenity::prelude::*;
 //use std::env;
+use serde::Deserialize;
+use std::fs;
 use std::fs::File;
 use std::io::Read;
 
-struct Handler;
+#[derive(Deserialize)]
+struct Config {
+    channel_id: u64,
+    role_id: u64,
+}
+
+struct Handler {
+    config: Config,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -24,8 +34,8 @@ impl EventHandler for Handler {
         if new.member.as_ref().unwrap().user.bot {
             return;
         }
-        let text_channel = ChannelId(890090820613603409);
-        let mention = RoleId(889106903836286987).mention();
+        let text_channel = ChannelId(self.config.channel_id);
+        let mention = RoleId(self.config.role_id).mention();
 
         if let Some(new_channel_id) = new.channel_id {
             let user_name = new.member.as_ref().unwrap().display_name();
@@ -54,7 +64,10 @@ impl EventHandler for Handler {
                 }
             } else {
                 // User joined new channel
-                let msg = format!("{}, {}({}) joined {}", mention, user_name, actual_name, channel_name);
+                let msg = format!(
+                    "{}, {}({}) joined {}",
+                    mention, user_name, actual_name, channel_name
+                );
                 // Send message to text channel
                 if let Err(why) = text_channel.say(&ctx.http, msg).await {
                     println!("{:?}", why);
@@ -88,6 +101,7 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
+    let config = load_config();
     let mut file = File::open("config.txt").expect("Fail to open");
     let mut token = String::new();
     file.read_to_string(&mut token).expect("Fail to read");
@@ -99,11 +113,16 @@ async fn main() {
         | GatewayIntents::MESSAGE_CONTENT;
 
     let mut client = Client::builder(&token, intents)
-        .event_handler(Handler)
+        .event_handler(Handler { config })
         .await
         .expect("Err creating client");
 
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
+}
+
+fn load_config() -> Config {
+    let config_str = fs::read_to_string("config.json").expect("Failed to read config file");
+    serde_json::from_str(&config_str).expect("Failed to parse config file")
 }
